619b52519ff611fbf0fc4a47d1cfb03c7d6fc03a
Matthew <Matt@nwfineartprinting.com>
2016-03-13 22:38:41 -0700

    Update lib.sh
    Adds in function to create symlinks.

--- lib.sh
+++ lib.sh
@@ -109,3 +109,24 @@ function require_apm() {
     fi
     ok
 }
+
+function symlinkifne {
+    running "$1"
+
+    if [[ -e ~/$1 ]]; then
+        # file exists
+        if [[ -L ~/$1 ]]; then
+            # it's already a simlink (could have come from this project)
+            echo -en '\tsimlink exists, skipped\t';ok
+            return
+        fi
+        # backup file does not exist yet
+        if [[ ! -e ~/.dotfiles_backup/$1 ]];then
+            mv $1 ~/.dotfiles_backup/
+            echo -en 'backed up saved...';
+        fi
+    fi
+    # create the link
+    ln -s $filepath/$1 ~/$1
+    echo -en '\tlinked';ok
+}
