bec0746e2752be271693044d33973e028102daa3
Matthew <Matt@nwfineartprinting.com>
2016-03-13 23:12:15 -0700

    Create repos.sh
    creates auto repo closing

---
+++ scripts/repos.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Projects
+git clone https://github.com/DS-Matt/laravel.git ~/Code/Projects/laravel
+git clone https://github.com/Diversified-Systems/Hercules.git ~/Code/Sites/Hercules
+
+#Cronus Repo - DS Enterprise
+git clone https://github.com/Diversified-Systems/Cronus.git ~/Code/Sites/Cronus
